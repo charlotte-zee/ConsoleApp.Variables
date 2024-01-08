e5d2095169b911445d387d97120fa41b9075a76c
Charlotte Zee <Charlotte Zee@DESKTOP-E556PH8>
2024-01-08 16:13:27 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Different Datatypes
+/*
+ 
+ text - string
+interger - int
+decimal - double, float, decimal
+logical - bool ( true or false )
+*/
+
+
+//string
+string name = "Charlotte Zee";
+Console.WriteLine(name);
+Console.WriteLine("I am " + name);
+Console.WriteLine($"They call me {name}");
+Console.WriteLine("I gave my self the name {0}", name);
+
+
+//interger
+int age = 25;
+int retirement = 11;
+int retirementAge = age + retirement;
+Console.WriteLine("My age is "+ age);
+Console.WriteLine("My retirement age is " + retirementAge);
+
+
+//logical bool ( ture or false )
+bool isRetired = false;
+Console.WriteLine("Ami I retired? " + isRetired);
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
